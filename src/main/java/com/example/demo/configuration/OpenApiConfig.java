15e8abda794c1332ebcc894cb0ed1a98eb46a031
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-02-20 19:51:49 +0100

    feat: add server in swagger annotations

--- src/main/java/com/example/demo/configuration/OpenApiConfig.java
+++ src/main/java/com/example/demo/configuration/OpenApiConfig.java
@@ -9,7 +9,9 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(info = @Info(contact = @io.swagger.v3.oas.annotations.info.Contact(name = "MyApp"), description = "MyApp", title = "MyApp", version = "1.0.0", license = @io.swagger.v3.oas.annotations.info.License(name = "Apache 2.0", url = "http://www.apache.org/licenses/LICENSE-2.0.html"), termsOfService = "http://swagger.io/terms/"
 
 ), servers = {
-                @io.swagger.v3.oas.annotations.servers.Server(description = "Localhost", url = "http://localhost:8080")
+                @io.swagger.v3.oas.annotations.servers.Server(description = "Localhost", url = "http://localhost:8080"),
+                @io.swagger.v3.oas.annotations.servers.Server(description = "Render Server", url = "https://ecofridge-back.onrender.com")
+                
 })
 
 @SecurityScheme(name = "Authorization", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
